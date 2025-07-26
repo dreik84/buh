4a825e693dd07e6cbb117ff5bc0ed3954a117f7b
dreik84 <dreik84@mail.ru>
2025-07-26 18:58:50 +0300

    Add ReportService.java

---
+++ src/main/java/com/example/ReportService.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class ReportService {
+
+    public void readAllMonthlyReports(int year) {
+
+    }
+
+    public void readYearlyReport(int year) {
+
+    }
+
+    public void compareReports() {
+
+    }
+
+    public void printMonthlyReportsInfo() {
+
+    }
+
+    public void printYearlyReportInfo() {
+
+    }
+}
