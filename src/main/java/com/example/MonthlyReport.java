a9cb21fb6b7494b1c2061b951fd38a65037b2eef
dreik84 <dreik84@mail.ru>
2025-07-26 11:53:29 +0300

    Update MonthlyReport.java

--- src/main/java/com/example/MonthlyReport.java
+++ src/main/java/com/example/MonthlyReport.java
@@ -1,10 +1,33 @@
 package com.example;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MonthlyReport {
-    String itemName;
-    boolean isExpense;
-    int quantity;
-    BigDecimal sumOfOne;
+
+    List<Record> list;
+
+    public MonthlyReport() {
+        list = new ArrayList<>();
+    }
+
+    public void addRecord(String itemName, Boolean isExpense, Integer quantity, BigDecimal sumOfOne) {
+        Record record = new Record(itemName, isExpense, quantity, sumOfOne);
+        list.add(record);
+    }
+
+    private static class Record {
+        String itemName;
+        Boolean isExpense;
+        Integer quantity;
+        BigDecimal sumOfOne;
+
+        Record(String itemName, Boolean isExpense, Integer quantity, BigDecimal sumOfOne) {
+            this.itemName = itemName;
+            this.isExpense = isExpense;
+            this.quantity = quantity;
+            this.sumOfOne = sumOfOne;
+        }
+    }
 }
