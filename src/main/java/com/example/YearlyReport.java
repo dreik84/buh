3c25eb00bbfe22bb5854f79bebc64f62e86f9152
dreik84 <dreik84@mail.ru>
2025-07-26 11:53:42 +0300

    Update YearlyReport.java

--- src/main/java/com/example/YearlyReport.java
+++ src/main/java/com/example/YearlyReport.java
@@ -1,9 +1,31 @@
 package com.example;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class YearlyReport {
-    int month;
-    BigDecimal amount;
-    boolean isExpense;
+
+    List<Record> list;
+
+    public YearlyReport() {
+        list = new ArrayList<>();
+    }
+
+    public void addRecord(Integer month, BigDecimal amount, Boolean isExpense) {
+        Record record = new Record(month, amount, isExpense);
+        list.add(record);
+    }
+
+    private static class Record {
+        Integer month;
+        BigDecimal amount;
+        Boolean isExpense;
+
+        Record(Integer month, BigDecimal amount, Boolean isExpense) {
+            this.month = month;
+            this.amount = amount;
+            this.isExpense = isExpense;
+        }
+    }
 }
