b1fbcf64a85993401d78aaec6377a64447112279
dreik84 <dreik84@mail.ru>
2025-07-26 18:58:19 +0300

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,13 +5,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ReportService service = new ReportService();
 
         printMenu();
         int input = scanner.nextInt();
 
         while (input != 0) {
 
-            // TODO
+            switch (input) {
+                case 1:
+                    System.out.print("Укажите год: ");
+                    service.readAllMonthlyReports(scanner.nextInt());
+                    break;
+                case 2:
+                    System.out.print("Укажите год: ");
+                    service.readYearlyReport(scanner.nextInt());
+                    break;
+                case 3:
+                    service.compareReports();
+                    break;
+                case 4:
+                    service.printMonthlyReportsInfo();
+                    break;
+                case 5:
+                    service.printYearlyReportInfo();
+                    break;
+                default:
+                    System.out.println("Неверно указан пункт меню");
+            }
 
             printMenu();
             input = scanner.nextInt();
