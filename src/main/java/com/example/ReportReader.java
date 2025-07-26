159906a42a8a5e89bdb7e2a9a6aa0ee163b2eb5a
dreik84 <dreik84@mail.ru>
2025-07-26 11:53:53 +0300

    Update ReportReader.java

--- src/main/java/com/example/ReportReader.java
+++ src/main/java/com/example/ReportReader.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -8,16 +9,21 @@ public class ReportReader {
 
     public YearlyReport getYearlyReport(int year) {
         String file = readFileContentsOrNull("src/main/resources/" + "y." + year + ".csv");
+        YearlyReport report = new YearlyReport();
 
         if (file != null) {
             String[] lines = file.split("\\n");
 
             for (String line : lines) {
                 String[] lineContents = line.split(",");
+                Integer month = Integer.parseInt(lineContents[0]);
+                BigDecimal amount = BigDecimal.valueOf(Long.parseLong(lineContents[1]));
+                Boolean isExpense = Boolean.parseBoolean(lineContents[2]);
+                report.addRecord(month, amount, isExpense);
             }
         }
 
-        return new YearlyReport();
+        return report;
     }
 
     private String readFileContentsOrNull(String path) {
