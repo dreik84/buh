4093a6d8919e2984aaf16f3ed5ace268c1720cd7
dreik84 <dreik84@mail.ru>
2025-07-27 18:57:15 +0300

    Update YearlyReport.java

--- src/main/java/com/example/YearlyReport.java
+++ src/main/java/com/example/YearlyReport.java
@@ -1,20 +1,37 @@
 package com.example;
 
+import java.lang.reflect.RecordComponent;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public class YearlyReport {
 
-    List<Record> list;
+    private List<Record> records;
 
     public YearlyReport() {
-        list = new ArrayList<>();
+        records = new ArrayList<>();
     }
 
     public void addRecord(Integer month, BigDecimal amount, Boolean isExpense) {
         Record record = new Record(month, amount, isExpense);
-        list.add(record);
+        records.add(record);
+    }
+
+    public BigDecimal getTotalByMonth(int month) {
+        BigDecimal total = BigDecimal.valueOf(0);
+
+        for (Record record : records) {
+            if (record.month == month) {
+                if (record.isExpense) {
+                    total.subtract(record.amount);
+                } else {
+                    total.add(record.amount);
+                }
+            }
+        }
+
+        return total;
     }
 
     private static class Record {
