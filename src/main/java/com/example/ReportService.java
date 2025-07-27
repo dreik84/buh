4f25a1f41545447c7e9699934865942839dd7d18
dreik84 <dreik84@mail.ru>
2025-07-27 12:23:43 +0300

    Update ReportService.java

--- src/main/java/com/example/ReportService.java
+++ src/main/java/com/example/ReportService.java
@@ -1,13 +1,26 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReportService {
 
-    public void readAllMonthlyReports(int year) {
+    private List<MonthlyReport> monthlyReports;
+    private List<YearlyReport> yearlyReports;
 
+    public ReportService() {
+        monthlyReports = new ArrayList<>();
+        yearlyReports = new ArrayList<>();
+    }
+
+    public void readAllMonthlyReports(int year) {
+        for (int i = 1; i < 12; i++) {
+            monthlyReports.add(ReportReader.getMouthlyReport(year, i));
+        }
     }
 
     public void readYearlyReport(int year) {
-
+        yearlyReports.add(ReportReader.getYearlyReport(year));
     }
 
     public void compareReports() {
